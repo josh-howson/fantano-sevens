8cb6dcd66cd72c12da75d1e582afdb5a7a532ddf
Josh Howson <52839079+josh-howson@users.noreply.github.com>
2024-06-02 04:27:05 -0700

    no zoom

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,7 +20,7 @@ export default defineNuxtConfig({
   app: {
     head: {
       meta: [
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no' },
       ],
     }
   }
