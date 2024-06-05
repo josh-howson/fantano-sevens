e549eb65aa451b3fb60e1d10fee8d9b622f1c0ea
Josh Howson <52839079+josh-howson@users.noreply.github.com>
2024-06-05 11:00:05 -0700

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,6 +2,17 @@ self.addEventListener('install', event => {
   console.log('Service worker installed');
 });
 
-self.addEventListener('fetch', event => {
-  console.log('Fetch event for ', event.request.url);
+self.addEventListener('fetch', function(event) {
+  if (event.request.url.startsWith(self.location.origin)) {
+    // Handle requests for own origin
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  } else {
+    // Ignore requests to external domains
+    event.respondWith(fetch(event.request));
+  }
 });
+
