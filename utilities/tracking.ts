8decfec3fff7f677d76cb5d66a2332a893a67645
josh-howson <52839079+josh-howson@users.noreply.github.com>
2024-08-05 13:20:56 -0700

    no tracking during development

--- utilities/tracking.ts
+++ utilities/tracking.ts
@@ -1,4 +1,8 @@
+import { ENVIRONMENT } from "~/types/Environment";
+
 export function trackEvent(type: string, params?: any) {
+  if (process.env.NODE_ENV === ENVIRONMENT.DEVELOPMENT) return;
+
   const gtag = (window as any).gtag;
   if (typeof gtag !== 'undefined') {
     gtag('event', type, params);
