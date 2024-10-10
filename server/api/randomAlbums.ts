b2e4bb193134779f1a0c72638a8e3555e9d1e81e
joshattrader <62081437+joshattrader@users.noreply.github.com>
2024-10-09 19:34:54 -0700

    update albums list, host albums locally

--- server/api/randomAlbums.ts
+++ server/api/randomAlbums.ts
@@ -1,13 +1,11 @@
 import { eventHandler, getQuery } from 'h3';
 import { FantanoAlbum } from '~/types/Album';
 import { getRandomAlbums } from '~/utilities/album';
-
-const REVIEWS_JSON_URL = 'https://raw.githubusercontent.com/garcia-santiago/fantanoScraper/master/master.json';
+import allReviewsStatic from '~/assets/all-reviews.json';
 
 type FantanoReviewsRaw = FantanoAlbum[];
 export default eventHandler(async (event) => {
-  const allAlbumsResponse = await fetch(REVIEWS_JSON_URL);
-  const allAlbums = await allAlbumsResponse.json() as FantanoReviewsRaw;
+  const allAlbums = allReviewsStatic as FantanoReviewsRaw;
   const query = getQuery(event);
   const minRating = query.minRating as string;
 
