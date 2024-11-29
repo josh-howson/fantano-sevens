68abf13206cab95422977621b245e8f375f1d80f
josh-howson <52839079+josh-howson@users.noreply.github.com>
2024-11-29 14:07:10 -0800

    try client only

--- app.vue
+++ app.vue
@@ -22,7 +22,7 @@ import { trackEvent } from '~/utilities/tracking';
 import IconSparkle from './components/icons/IconSparkle.vue';
 import IconLightbulb from './components/icons/IconLightbulb.vue';
 import useVibration from './composables/useVibration';
-import useAudioPlayer from './composables/useAudioPlayer';
+import useAudioPlayer from './composables/useAudioPlayer.client';
 
 const SHUFFLE_DURATION = 4000;
 
