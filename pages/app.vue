5d2fd3e3c5e0cf1f02279367990a6d2a2d6bce4a
josh-howson <52839079+josh-howson@users.noreply.github.com>
2025-01-19 13:37:25 -0800

    initialize landing page

---
+++ pages/app.vue
@@ -0,0 +1,495 @@
+<script setup lang="ts">
+import { ref, computed, watch, onMounted } from 'vue';
+import type { Album, HistoryAlbum, ShuffleStatus } from '~/types/Album';
+import { preloadImage } from '~/utilities/image';
+import {
+  addToHistory,
+  getAlbumHistory,
+  updateLikeStatus,
+  updateLogStatus,
+  removeFromHistory,
+  incrementLifetimeSpins,
+  getLifetimeSpins,
+} from '~/utilities/history';
+import { getMinRatingFromCookie, setMinRatingCookie } from '~/utilities/preference';
+import { formatAlbumTitleAndArtist, getAlbumImage } from '~/utilities/album';
+import IconHistory from '~/components/icons/IconHistory.vue';
+import HistoryView from '~/components/views/HistoryView.vue';
+import SettingsView from '~/components/views/SettingsView.vue';
+import IconInstall from '~/components/icons/IconInstall.vue';
+import IconGear from '~/components/icons/IconGear.vue';
+import { trackEvent } from '~/utilities/tracking';
+import IconSparkle from '@/components/icons/IconSparkle.vue';
+import IconLightbulb from '@/components/icons/IconLightbulb.vue';
+import useVibration from '@/composables/useVibration';
+
+const SHUFFLE_DURATION = 4000;
+
+const { vibrate } = useVibration();
+
+const view: Ref<'picker' | 'history' | 'settings'> = ref('picker');
+const minRating = ref(7);
+const nextRandomAlbums: Ref<Album[]> = ref([]);
+const randomAlbums: Ref<Album[]> = ref([]);
+const currentAlbum: Ref<Album | null> = ref(null);
+const shuffleStatus: Ref<ShuffleStatus> = ref('init');
+const shuffleIndex = ref(0);
+const albumHistory: Ref<HistoryAlbum[]> = ref([]);
+const loggedAlbums = computed(() => albumHistory.value.filter(album => !!album.logged));
+const deferredPrompt = ref();
+const isInstallable = ref(false);
+const isPromptingToInstallPwa = ref(false);
+const sessionShuffleCount = ref(0);
+const pwaPickAgainClicked = ref(false);
+
+const { data, error, status, refresh } = useFetch<Album[]>('/api/randomAlbums', {
+  method: 'POST',
+  body: {
+    minRating: minRating,
+    loggedAlbums: loggedAlbums,
+  },
+  cache: 'no-cache',
+  watch: false,
+});
+
+const isShowBigButton = computed(() => shuffleStatus.value === 'init' && !isPromptingToInstallPwa.value);
+const isBigButtonDisabled = computed(() => status.value === 'pending');
+const bigButtonText = computed(() => status.value === 'pending' ? 'loading...' : 'pick a random album');
+
+watch(data, async (newValue) => {
+  if (newValue) {
+    nextRandomAlbums.value = newValue;
+    try {
+      const preloadPromises = nextRandomAlbums.value.map((album) => {
+        const albumCoverUrl = getAlbumImage(album, 'medium');
+        return albumCoverUrl ? preloadImage(albumCoverUrl.url) : Promise.resolve();
+      });
+
+      await Promise.all(preloadPromises)
+    } catch (error) {
+      console.error('There was an error preloading the image:', error);
+    }
+  }
+}, { immediate: true });
+
+watch([status, error], (newValue) => {
+  if (status.value === 'error')
+    console.error('There has been an error!', newValue);
+}, { immediate: true });
+
+let shuffleInterval: NodeJS.Timeout;
+
+const showFinalAlbum = () => {
+  shuffleInterval && clearInterval(shuffleInterval);
+  shuffleStatus.value = 'picked';
+};
+
+const applyPreloadedAlbums = () => {
+  randomAlbums.value = nextRandomAlbums.value;
+  refresh();
+};
+
+const shuffleByOne = () => {
+  shuffleIndex.value = Math.floor(Math.random() * randomAlbums.value.length);
+  if (shuffleIndex.value < randomAlbums.value.length - 1)
+    shuffleIndex.value++;
+  else
+    shuffleIndex.value = 0;
+  currentAlbum.value = randomAlbums.value[shuffleIndex.value];
+  vibrate('shortest');
+};
+
+const getShouldPromptToInstall = (): boolean => {
+  const spinsToPromptOn = [7, 20, 50];
+  if (!isInstallable.value) return false;
+
+  const lifetimeSpins = getLifetimeSpins();
+  if (
+    !spinsToPromptOn.includes(lifetimeSpins) ||
+    // also prompt every 125
+    lifetimeSpins > 0 &&
+    lifetimeSpins % 125 === 0
+  ) return false;
+
+  return true;
+};
+
+const handleShuffle = () => {
+  incrementLifetimeSpins();
+  isPromptingToInstallPwa.value = getShouldPromptToInstall();
+  if (isPromptingToInstallPwa.value) {
+    // skip shuffle if prompting to install
+    return;
+  }
+  applyPreloadedAlbums();
+  shuffleStatus.value = 'shuffling';
+  shuffleByOne();
+  setTimeout(() => {
+    showFinalAlbum();
+    sessionShuffleCount.value++;
+    trackEvent('shuffle', {
+      album: currentAlbum.value
+        ? formatAlbumTitleAndArtist(currentAlbum.value)
+        : null,
+      minScore: minRating.value,
+      sessionShuffleCount: sessionShuffleCount.value,
+    });
+  }, SHUFFLE_DURATION);
+};
+
+const handleStream = (album: HistoryAlbum, historyAdd: boolean = false) => {
+  const spotifyUrl = album.spotifyUrl;
+  window.open(spotifyUrl, '_blank', 'noopener,noreferrer');
+
+  if (historyAdd) {
+    handleAddAlbumToHistory(album);
+    syncAlbumHistoryRef();
+  }
+  trackEvent('stream', {
+    album: currentAlbum.value
+      ? formatAlbumTitleAndArtist(currentAlbum.value)
+      : null,
+  });
+};
+
+const handleAlbumFlip = () => {
+  shuffleByOne();
+};
+
+const syncAlbumHistoryRef = () => {
+  albumHistory.value = getAlbumHistory();
+};
+
+const handleShowHistory = () => {
+  view.value = 'history';
+}
+
+const handleCloseHistory = () => {
+  view.value = 'picker';
+};
+
+const handleShowSettings = () => {
+  view.value = 'settings';
+};
+
+const handleInstall = async () => {
+  deferredPrompt.value.prompt();
+  const { outcome } = await deferredPrompt.value.userChoice;
+  isInstallable.value = !outcome;
+  trackEvent('install');
+};
+
+const handleCloseSettings = () => {
+  view.value = 'picker';
+};
+
+const handleAddAlbumToHistory = (album: HistoryAlbum) => {
+  addToHistory(album);
+  syncAlbumHistoryRef();
+  trackEvent('history-add', {
+    album: currentAlbum.value
+      ? formatAlbumTitleAndArtist(currentAlbum.value)
+      : null,
+  });
+};
+
+const handleLogAlbum = (album: HistoryAlbum, value: boolean) => {
+  updateLogStatus(album, value);
+  syncAlbumHistoryRef();
+  trackEvent('log', {
+    album: currentAlbum.value
+      ? formatAlbumTitleAndArtist(currentAlbum.value)
+      : null,
+    logged: value,
+  });
+}
+
+const handleLikeAlbum = (album: HistoryAlbum, value: boolean) => {
+  updateLikeStatus(album, value);
+  syncAlbumHistoryRef();
+  trackEvent('like', {
+    album: currentAlbum.value
+      ? formatAlbumTitleAndArtist(currentAlbum.value)
+      : null,
+    liked: value,
+  });
+}
+
+const handleRemoveFromHistory = (album: HistoryAlbum) => {
+  removeFromHistory(album);
+  syncAlbumHistoryRef();
+  trackEvent('history-remove', {
+    album: currentAlbum.value
+      ? formatAlbumTitleAndArtist(currentAlbum.value)
+      : null,
+  });
+}
+
+const handlePwaPromptPickAgain = () => {
+  pwaPickAgainClicked.value = true;
+  setTimeout(() => {
+    handleShuffle();
+    pwaPickAgainClicked.value = false;
+  }, 3000);
+}
+
+watch([minRating, view], () => {
+  setMinRatingCookie(minRating.value);
+  // delay fetching until back on picker view
+  if (view.value === 'picker') {
+    refresh();
+  }
+});
+
+onMounted(syncAlbumHistoryRef);
+
+onBeforeMount(() => {
+  const minRatingFromCookie = getMinRatingFromCookie();
+  if (minRatingFromCookie) minRating.value = minRatingFromCookie;
+});
+
+onBeforeMount(() => {
+  window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    deferredPrompt.value = e;
+    isInstallable.value = true;
+  });
+});
+</script>
+
+<template>
+  <div
+    class="page-layout"
+    v-if="view === 'picker'"
+    :style="{ '--shuffle-duration': `${SHUFFLE_DURATION}ms` }"
+  >
+    <div :class="[
+      'top-controls',
+      shuffleStatus === 'shuffling' && 'shuffling',
+    ]">
+      <button
+        v-if="isInstallable"
+        class="install button-icon button-secondary"
+        @click="handleInstall"
+        title="install app"
+        aria-label="install app"
+      >
+        <IconInstall />
+      </button>
+
+      <button
+        class="settings button-icon button-secondary"
+        @click="handleShowSettings"
+        title="settings"
+        aria-label="settings">
+        <IconGear />
+      </button>
+
+      <button
+        class="show-history button-icon button-secondary"
+        @click="handleShowHistory"
+        aria-label="History"
+        title="history"
+      >
+        <IconHistory />
+
+        <div
+          v-if="albumHistory.length"
+          class="history-count">
+          {{albumHistory.length < 10 ? albumHistory.length : '9+'}}
+        </div>
+      </button>
+    </div>
+
+    <button
+      class="button-big button-primary"
+      @click="handleShuffle"
+      :disabled="isBigButtonDisabled"
+      v-if="isShowBigButton">
+      {{ bigButtonText }}
+    </button>
+
+    <div
+      class="pwa-install-prompt"
+      v-if="isPromptingToInstallPwa && !pwaPickAgainClicked"
+    >
+      <IconSparkle height="32" width="32" />
+
+      <div class="pwa-title">discover any new music yet?</div>
+
+      <div>it looks like you’re enjoying the site. for an even better experience, install it to your device.</div>
+
+
+      <button
+        class="button-medium button-primary"
+        @click="handleInstall"
+        v-if="isInstallable"
+      >
+        <IconInstall />
+
+        <span>install</span>
+      </button>
+    </div>
+
+    <Alert v-if="isPromptingToInstallPwa && pwaPickAgainClicked">
+      <template #icon>
+        <IconLightbulb />
+      </template>
+
+      <div>if you want to install it later, press the <IconInstall /> button in the top left.</div>
+    </Alert>
+
+    <KeepAlive v-if="!isPromptingToInstallPwa">
+      <AlbumInfo
+        v-if="currentAlbum"
+        :album="currentAlbum"
+        :shuffleStatus="shuffleStatus"
+        :albumHistory="albumHistory"
+        @flip="handleAlbumFlip" />
+    </KeepAlive>
+
+    <div class="bottom-controls">
+      <KeepAlive>
+        <AlbumActions
+          v-if="currentAlbum && !isPromptingToInstallPwa"
+          :albumHistory="albumHistory"
+          :album="currentAlbum"
+          :shuffleStatus="shuffleStatus"
+          :can-shuffle="status !== 'pending'"
+          @history-add="handleAddAlbumToHistory"
+          @shuffle="handleShuffle"
+          @stream="handleStream"
+        />
+      </KeepAlive>
+
+      <button
+        :class="['pick-again button-big button-secondary']"
+        @click="handlePwaPromptPickAgain"
+        v-if="isPromptingToInstallPwa && !pwaPickAgainClicked"
+        >
+          <IconsIconRedo />
+
+          <span>pick again</span>
+      </button>
+    </div>
+  </div>
+
+  <HistoryView
+    :album-history="albumHistory"
+    @close="handleCloseHistory"
+    @remove="handleRemoveFromHistory"
+    @log="handleLogAlbum"
+    @like="handleLikeAlbum"
+    @stream="handleStream"
+    v-else-if="view === 'history'"
+  />
+
+  <SettingsView
+    @close="handleCloseSettings"
+    v-model="minRating"
+    v-else-if="view === 'settings'"
+  />
+
+  <div v-else>an error occured :(</div>
+</template>
+
+<style scoped>
+.top-controls {
+  display: flex;
+  align-items: center;
+  justify-content: end;
+  gap: var(--spacing-1);
+  margin-inline-start: auto;
+  margin-block-end: auto;
+  width: 100%;
+  min-height: 40px;
+
+  &.shuffling>button {
+    opacity: 0;
+    scale: 0;
+    transition: all var(--transition-duration) var(--easing);
+  }
+}
+
+.top-controls .install {
+  margin-inline-end: auto;
+}
+
+.show-history {
+  position: relative;
+}
+
+.history-count {
+  width: 20px;
+  height: 20px;
+  border-radius: 999px;
+  background: var(--on-surface);
+  border: 2px solid var(--bg-surface-light);
+  color: var(--bg-surface-light);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  right: 0;
+  font-size: 11px;
+  font-weight: bold;
+  text-align: center;
+  margin-block-start: -6px;
+  margin-inline-end: -6px;
+}
+
+.history {
+  padding: var(--spacing-1);
+  display: flex;
+  flex-flow: column nowrap;
+  gap: var(--spacing);
+  height: 100dvh;
+}
+
+.bottom-controls {
+  display: flex;
+  gap: var(--spacing-2);
+  align-items: center;
+  flex-direction: column;
+  margin-block-start: auto;
+}
+
+.pwa-install-prompt {
+  padding: var(--spacing-2);
+  gap: var(--spacing-1);
+  border-radius: 16px;
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: start;
+  background: var(--bg-surface-light);
+  border: 3px solid var(--on-surface);
+  max-width: 400px;
+}
+
+.pwa-title {
+  font-weight: bold;
+  font-size: 20px;
+}
+
+.install-later {
+  padding: var(--spacing-2);
+  border-radius: 16px;
+  background: var(--bg-surface-light);
+  border: 3px solid var(--on-surface);
+  max-width: 400px;
+}
+
+.pick-again {
+  opacity: 0;
+  visibility: hidden;
+  animation: delayed-show 2s forwards;
+  animation-delay: 3s;
+}
+
+@media (max-height: 700px) {
+  .bottom-controls {
+    gap: var(--spacing-1);
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,213 @@
+<script setup lang="ts">
+useHead({
+  link: [
+    {
+      rel: 'preload',
+      href: 'https://i.ibb.co/7jrP03m/home-hero.png',
+      as: 'image',
+      type: 'image/png',
+    },
+  ],
+});
+</script>
+
+<template>
+  <main class="home-wrap">
+    <header class="header">
+      <NuxtLink to="/">fantanosevens</NuxtLink>
+      
+      <!-- <button class="button-icon button-secondary" title="install"><IconInstall /></button> -->
+    </header>
+    <section class="hero">
+      <div class="hero-content">
+        <h1>the fastest way to discover top-rated music.</h1>
+
+        <p>with a single click, spin to get new music recommendations spanning all genres.</p>
+
+        <NuxtLink to="/app" class="button-big button-primary">discover music now &rarr;</NuxtLink>
+      </div>
+
+      <div class="hero-visual">
+        <img alt="" width="540" height="884" src="https://i.ibb.co/7jrP03m/home-hero.png" />
+      </div>
+    </section>
+
+    <section class="good-stuff">
+      <div class="content">
+        <h2>only the good stuff.</h2>
+
+        <p>all album recommendations are vetted by trusted music critics. no more sifting through mediocre playlists. you can hit play with confidence you’re listening to the best.</p>
+
+        <NuxtLink to="/app">discover music now &rarr;</NuxtLink>
+      </div>
+
+      <div class="visual">
+        <img alt="" width="437" height="233" src="https://i.ibb.co/HCvWMcz/home-good-stuff.png" loading="lazy" />
+      </div>
+    </section>
+
+    <section class="keep-track">
+      <div class="content">
+        <h2>keep track of your favorites.</h2>
+
+        <p>we save the albums you listen to, so you can come back to log, save or stream your favorite albums again anytime.</p>
+
+        <NuxtLink to="/app">discover music now &rarr;</NuxtLink>
+      </div>
+
+      <div class="visual">
+        <img alt="" width="419" height="714" src="https://i.ibb.co/7KRb2TS/home-keep-track.png" loading="lazy" />
+      </div>
+    </section>
+
+    <section class="easy-and-free">
+      <h2>it's easy and free.</h2>
+
+      <p>so start listening to something great right now.</p>
+
+      <NuxtLink to="/app" class="button-big button-primary">get started</NuxtLink>
+    </section>
+  </main>
+</template>
+
+<style scoped>
+.home-wrap {
+  display: flex;
+  flex-flow: column nowrap;
+  gap: 32px;
+  padding: 16px;
+  p {
+    line-height: 1.5;
+  }
+  a.button-big {
+    display: inline-flex;
+    text-decoration: none;
+  }
+  @media (max-width: 1023px) {
+    .button-big {
+      font-size: 16px;
+      padding: 16px 32px;
+    }
+  }
+  @media (min-width: 1024px) {
+    display: grid;
+    grid-template-columns: minmax(16px, 1fr) minmax(0, 150px) minmax(0, 800px) minmax(0, 150px) minmax(16px, 1fr);
+  }
+  p {
+    max-width: 40ch;
+  }
+}
+.home-wrap > * {
+  grid-column: 3;
+}
+.header {
+  grid-column: 2 / -2;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 8px;
+  padding: 24px 0;
+  a {
+    text-decoration: none;
+    font-size: 20px;
+    @media (min-width: 1024px) {
+      font-size: 32px;
+    }
+  }
+}
+.hero {
+  grid-column: 2 / -2;
+  display: flex;
+  gap: 64px;
+  flex-direction: column;
+  @media (min-width: 1024px) {
+    align-items: center;
+    flex-direction: row;
+    min-height: 90vh;
+    p {
+      font-size: 24px;
+    }
+  }
+}
+.hero-content {
+  h1 {
+    font-size: 32px;
+    line-height: 1.2;
+    max-width: 30ch;
+    text-wrap: balance;
+    text-wrap: pretty;
+  }
+  @media (min-width: 1024px) {
+    flex: 3 3 0;
+    h1 {
+      font-size: 52px;
+    }
+  }
+}
+.hero-visual {
+  width: 100%;
+  text-align: center;
+  img {
+    transform: translate(0%, 0%) scale(1) rotate(0deg);
+    transition: transform .3s ease;
+    width: 100%;
+    max-width: 600px;
+    height: auto;
+    &:hover {
+      transform: translate(0%, -2%) scale(1.1) rotate(1deg);
+    }
+  }
+  @media (min-width: 1024px) {
+    flex: 2 2 0;
+    width: 0;
+  }
+}
+.good-stuff,
+.keep-track {
+  display: flex;
+  gap: 32px;
+  align-items: center;
+  flex-flow: column nowrap;
+  h2 {
+    @media (min-width: 1024px) {
+      font-size: 32px;
+    }
+  }
+  a {
+    font-size: 20px;
+  }
+  .visual {
+    img {
+      width: 100%;
+      height: auto;
+    }
+  }
+  @media (min-width: 1024px) {
+    gap: 64px;
+    flex-flow: row nowrap;
+  }
+}
+.good-stuff {
+  @media (min-width: 1024px) {
+    flex-direction: row-reverse;
+  }
+}
+.keep-track {
+  margin-top: 64px;
+}
+.easy-and-free {
+  text-align: center;
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  padding-bottom: 200px;
+  @media (min-width: 1024px) {
+    h2 {
+      font-size: 52px
+    }
+    p {
+      font-size: 24px;
+    }
+  }
+}
+</style>
