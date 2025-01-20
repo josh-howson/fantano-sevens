763532ca601ba1d65de1589caa4aa0b56da82513
joshattrader <52839079+josh-howson@users.noreply.github.com>
2025-01-20 07:24:25 -0800

    remove adsense

--- app.vue
+++ app.vue
@@ -1,13 +1,3 @@
-<script lang="ts" setup>
-  onMounted(() => {
-    const script = document.createElement('script');
-    script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4822177946089338';
-    script.async = true;
-    script.crossOrigin = 'anonymous';
-    document.body.appendChild(script);
-  });
-</script>
-
 <template>
   <NuxtRouteAnnouncer />
   <NuxtPage />
