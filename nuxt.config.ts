7f26dbe037a7e5f04f3e13e366d06ba1c461b309
josh-howson <52839079+josh-howson@users.noreply.github.com>
2025-01-19 17:28:34 -0800

    page transition for fun !

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,6 +25,7 @@ export default defineNuxtConfig({
   ],
 
   app: {
+    pageTransition: { name: 'page', mode: 'out-in' },
     head: {
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no, viewport-fit=cover' },
